305b96e9f190b798796f2214ab6921eeb0997267
alhigaylan <mjilan30@gmail.com>
2026-03-12 02:20:56 -0400

    FFPep: re-introduce trace count in a simplified way. Hypotheses with 2 gets 0 score. Anything above 3 gets 1.0

--- src/openms/source/FEATUREFINDER/FeatureFindingPeptide.cpp
+++ src/openms/source/FEATUREFINDER/FeatureFindingPeptide.cpp
@@ -449,7 +449,8 @@ namespace OpenMS
           double total_pair_score(0.0);
           if (rt_score > 0.0 && mz_score > 0.0 && int_score > 0.0)
           {
-            total_pair_score = (rt_score + mz_score + int_score) / 3.0;
+            const double trace_count_score = (fh_tmp.getSize() + 1 > 2) ? 1.0 : 0.0;
+            total_pair_score = (rt_score + mz_score + int_score + trace_count_score) / 4.0;
           }
           if (total_pair_score > best_so_far)
           {
